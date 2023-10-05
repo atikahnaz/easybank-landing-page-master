26a6d7a472b3aca5b7d6b2d2c82ad4039d4427d2
Atikah <atikahnazir6@gmail.com>
2023-10-05 14:00:51 +0800

    grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,89 @@
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
 import "./App.css";
 
-function Navbar() {
+function MenuHamburger() {
   return (
-    <nav className="w-full bg-white fixed z-10 p-5 flex items-center  md:justify-between">
-      <img src="/images/logo.svg" alt="logo" />
-      <ul className="hidden md:flex  ">
-        <li>
+    <div className="flex items-start justify-center absolute w-full z-20 top-10  ">
+      <ul className="w-3/4 h-1/2 z-20 flex flex-col items-center justify-evenly bg-slate-300">
+        <li className="pl-2 pt-8 pb-3">
           <a href="#">Home</a>
         </li>
-        <li className="pl-2">
+        <li className="pl-2 py-3">
           <a href="#">About</a>
         </li>
-        <li className="pl-2">
+        <li className="pl-2 py-3">
           <a href="#">Contact</a>
         </li>
-        <li className="pl-2">
+        <li className="pl-2 py-3">
           <a href="#">Blog</a>
         </li>
-        <li className="pl-2">
+        <li className="pl-2 py-3">
           <a href="#">Careers</a>
         </li>
       </ul>
-      <button
-        type="button"
-        className="hidden md:flex bg-Lime-Green px-7 py-2 rounded-3xl text-base font-bold text-White"
-      >
-        Request Invite
-      </button>
-      <img
-        className="absolute right-5 md:hidden"
-        src="/images/icon-hamburger.svg"
-        alt="hamburger"
-      />
-    </nav>
+    </div>
+  );
+}
+
+function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function HamburgerClick() {
+    setShowMenu(!showMenu);
+  }
+  const changeBackground = () => {
+    const rootElement = document.getElementById("root");
+    if (rootElement) {
+      rootElement.style.backgroundColor = "red";
+    }
+  };
+
+  return (
+    <>
+      <nav className="w-screen bg-white fixed z-50 p-5 flex items-center justify-between">
+        <img src="/images/logo.svg" alt="logo" />
+
+        <button
+          type="button"
+          className="hidden lg:flex bg-Lime-Green px-7 py-2 rounded-3xl text-base font-bold text-White"
+        >
+          Request Invite
+        </button>
+        <img
+          className="absolute right-5 md:hidden"
+          src="/images/icon-hamburger.svg"
+          alt="hamburger"
+          onClick={HamburgerClick}
+        />
+      </nav>
+      {showMenu && <MenuHamburger />}
+    </>
   );
 }
 
 function HeaderPage() {
   return (
-    <header className="w-screen relative bg-Very-Light-Gray">
+    <header className="w-full flex flex-col items-center bg-Very-Light-Gray lg:relative lg:flex-row-reverse lg:top-20">
       <img
-        className="w-full h-auto relative"
+        className="w-full h-auto relative lg:hidden"
         src="/images/bg-intro-mobile.svg"
         alt="header"
       />
+      <div className="overflow-x-hidden">
+        <img
+          className="hidden  lg:flex relative max-w-full -top-32 -right-32"
+          src="public/images/bg-intro-desktop.svg"
+          alt="header"
+        />
+      </div>
+
       <img
-        className="max-w-full absolute top-[-4rem] px-3"
+        className="max-w-full absolute top-[-4rem] px-3 lg:-right-36 lg:z-40"
         src="/images/image-mockups.png"
         alt="mockup"
       />
-      <div className="px-4">
+      <div className="px-4 lg:pl-16 lg:w-1/2">
         <h1 className="relative -top-8 font-customSans text-center text-5xl leading-10 text-Dark-Blue">
           Next generation digital banking
         </h1>
@@ -73,56 +107,57 @@ function HeaderPage() {
 
 function SectionPage() {
   return (
-    <section className=" bg-Light-Grayish-Blue py-16 font-customSans">
-      <div>
-        <h1 className=" text-3xl text-center px-9 pb-5 text-Dark-Blue">
+    <section className=" bg-Light-Grayish-Blue py-16 font-customSans lg:grid lg:grid-cols-4 lg:grid-rows-2 lg:relative lg:top-8 lg:z-20 lg:px-20">
+      <div className="lg:col-span-4 lg:top-40 lg:w-1/2 lg:mt-12 lg:h-1/6">
+        <h1 className=" text-3xl text-center px-9 pb-5 text-Dark-Blue lg:text-left lg:px-0 lg:m-0 lg:text-5xl">
           Why choose Easybank?
         </h1>
-        <p className="text-base text-center px-2 pb-5 text-Grayish-Blue">
+        <p className="text-base text-center px-2 pb-5 text-Grayish-Blue lg:text-left lg:px-0 lg:pb-0 lg:text-lg">
           We leverage Open Banking to turn your bank account into your financial
           hub. Control your finances like never before.
         </p>
       </div>
-      <div className="flex flex-col items-center pt-10">
+
+      <div className="flex flex-col items-center pt-10 lg:items-start">
         <img
           className=" max-w-min"
           src="/images/icon-online.svg"
           alt="online"
         />
-        <h3 className="text-xl text-center px-2 pt-5 text-Dark-Blue">
+        <h3 className="text-xl text-center px-2 pt-5 text-Dark-Blue lg:text-left">
           Online Banking
         </h3>
-        <p className="text-base text-center px-2 pt-5 text-Grayish-Blue">
+        <p className="text-base text-center px-2 pt-5 text-Grayish-Blue lg:text-left">
           Our modern web and mobile applications allow you to keep track of your
           finances wherever you are in the world.
         </p>
       </div>
-      <div className="flex flex-col items-center pt-10">
+      <div className="flex flex-col items-center pt-10 lg:items-start">
         <img src="/images/icon-budgeting.svg" alt="budget" />
         <h3 className="text-xl text-center px-2 pt-5 text-Dark-Blue">
           Simple Budgeting
         </h3>
-        <p className="text-base text-center px-2 pt-5 text-Grayish-Blue">
+        <p className="text-base text-center px-2 pt-5 text-Grayish-Blue lg:text-left">
           See exactly where your money goes each month. Receive notifications
           when you’re close to hitting your limits.
         </p>
       </div>
-      <div className="flex flex-col items-center pt-10">
+      <div className="flex flex-col items-center pt-10 lg:items-start">
         <img src="/images/icon-onboarding.svg" alt="onboarding" />
         <h3 className="text-xl text-center px-2 pt-5 text-Dark-Blue">
           Fast Onboarding
         </h3>
-        <p className="text-base text-center px-2 pt-5 text-Grayish-Blue">
+        <p className="text-base text-center px-2 pt-5 text-Grayish-Blue lg:text-left">
           We don’t do branches. Open your account in minutes online and start
           taking control of your finances right away.
         </p>
       </div>
-      <div className="flex flex-col items-center pt-10">
+      <div className="flex flex-col items-center pt-10 lg:items-start">
         <img src="/images/icon-api.svg" alt="api" />
         <h3 className="text-xl text-center px-2 pt-5 text-Dark-Blue">
           Open API
         </h3>
-        <p className="text-base text-center px-2 pt-5 text-Grayish-Blue">
+        <p className="text-base text-center px-2 pt-5 text-Grayish-Blue lg:text-left">
           Manage your savings, investments, pension, and much more from one
           account. Tracking your money has never been easier.
         </p>
